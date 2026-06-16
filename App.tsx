20c5ef130e816488ab025a94f77d658f1f388947
serenbeam <92lailikumala@gmail.com>
2026-06-16 11:37:02 +0700

    [navigation] setup and create content each screen

--- App.tsx
+++ App.tsx
@@ -1,30 +1,7 @@
-import {StyleSheet, Text} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
-const App = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>AI Grammar Checker</Text>
-      <Text style={styles.subTitle}>Project setup completed succesfully</Text>
-    </SafeAreaView>
-  );
-};
+import AppNavigator from '@/navigation/AppNavigator';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F8FAFC'
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: '700'
-  },
-  subTitle: {
-    marginTop: 12,
-    fontSize: 16,
-    color: '#64748B'
-  }
-})
+const App = () => {
+  return <AppNavigator/>
+};
 
 export default App;
